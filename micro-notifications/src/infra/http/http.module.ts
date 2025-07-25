b8c998613499775f4179f2eface707bfe1b0a2de
Felipe Dornelles <lipe.dornelles@hotmail.com>
2025-07-25 15:25:08 -0300

    fix: name controller

--- micro-notifications/src/infra/http/http.module.ts
+++ micro-notifications/src/infra/http/http.module.ts
@@ -2,7 +2,7 @@ import { OrderNotify } from "@/domain/application/use-cases/order-notify";
 import { Module } from "@nestjs/common";
 import { EmailModule } from "../email/email.module";
 import { OrderCreatedNotifyController } from "./controllers/order-created-notify.controller";
-import { OrderDeliveriedNotifyController } from "./controllers/order-deliveied-notify.controller";
+import { OrderDeliveriedNotifyController } from "./controllers/order-deliveried-notify.controller";
 import { OrderPaiedNotifyController } from "./controllers/order-paied-notify.controller";
 
 @Module({
