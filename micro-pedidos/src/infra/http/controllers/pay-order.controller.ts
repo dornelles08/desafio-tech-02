2ec58d8fad38794fa0df46f5a774824e2f6b3fff
Felipe Dornelles <lipe.dornelles@hotmail.com>
2025-07-25 12:59:36 -0300

    feat: adjust the event handler in the PayOrderController

--- micro-pedidos/src/infra/http/controllers/pay-order.controller.ts
+++ micro-pedidos/src/infra/http/controllers/pay-order.controller.ts
@@ -4,14 +4,12 @@ import { Controller } from "@nestjs/common";
 import { Ctx, EventPattern, Payload, RmqContext } from "@nestjs/microservices"; // Importe os decoradores [3]
 
 interface OrderMessagePayload {
-  pattern: string;
-  data: {
-    value: number;
-    customerEmail: string;
-    customerName: string;
-    status: string;
-    id: string;
-  };
+  id: string;
+  value: number;
+  customerEmail: string;
+  customerName: string;
+  status: string;
+  createdAt: string;
 }
 
 @Controller()
@@ -19,22 +17,16 @@ export class PayOrderController {
   constructor(private readonly payOrderUseCase: PayOrderUseCase) {}
 
   @EventPattern("order.paied")
-  async handleOrderCreated(
-    @Payload() data: OrderMessagePayload["data"],
-    @Ctx() context: RmqContext
-  ) {
+  async handleOrderCreated(@Payload() data: OrderMessagePayload, @Ctx() context: RmqContext) {
     const channel = context.getChannelRef();
     const originalMsg = context.getMessage();
 
-    // if (originalMsg.fields.routingKey !== "create-order") {
-    //   return;
-    // }
-
     try {
       console.log(` Recebido evento 'order.paied' para o pedido ID: ${data.id}`);
-      // console.log(` Detalhes do pedido: ${JSON.stringify(data)}`);
 
-      console.log(` Pedido ${data.id} processado com sucesso.`);
+      await this.payOrderUseCase.execute({
+        orderId: data.id,
+      });
 
       channel.ack(originalMsg);
     } catch (error) {
